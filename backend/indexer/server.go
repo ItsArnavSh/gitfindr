54b65e863b0e4af2002e50967a9c8fb0664f5a29
bhavyakhatri2703 <khatribhavya2703@gmail.com>
2025-02-22 17:05:32 +0530

    post request

--- backend/indexer/server.go
+++ backend/indexer/server.go
@@ -6,8 +6,26 @@ import (
 
 func setupServer() *fiber.App {
 	app := fiber.New()
-	app.Get("/results", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{"message": "Results!"})
+
+	app.Post("/search", func(c *fiber.Ctx) error {
+
+		type RequestBody struct {
+			Query string `json:"query"`
+		}
+
+		var body RequestBody
+		if err := c.BodyParser(&body); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Invalid request body",
+			})
+		}
+
+		results := bm25(body.Query)
+
+		return c.JSON(fiber.Map{
+			"results": results,
+		})
 	})
+
 	return app
 }
