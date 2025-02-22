1b29bfa60d6bc91deb569dd927daa805a1a49d99
bhavyakhatri2703 <khatribhavya2703@gmail.com>
2025-02-22 11:31:44 +0530

    frontendInitialCommit

---
+++ frontend/src/search.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState , useEffect } from "react";
+
+export default function Search()
+{
+    return (
+        <div className="min-h-screen w-screen bg-[#0D1117] flex-col justify-center items-center">
+            <div className="text-6xl font-extrabold text-white text-center font-['Roboto']">
+                GitFindr
+            </div>
+            <div>
+            <form>
+                <input></input>
+            </form>
+            </div>
+            <div>
+                <img></img>
+            </div>
+
+        </div>
+    )
+}
